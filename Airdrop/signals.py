94c59fc6da1e3ca6b389b3c85f1036712810cdcc
Mahadi Hasan Raju <97337176+mhraju69@users.noreply.github.com>
2025-07-30 04:43:28 +0600

    Update signals.py

--- Airdrop/signals.py
+++ Airdrop/signals.py
@@ -1,10 +1,18 @@
-from django.db.models.signals import post_save
-from django.contrib.auth.models import User
-from django.dispatch import receiver
-from .models import *
+# Airdrop/signals.py
 
+import sys
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth import get_user_model
+from .models import Participant
+
+User = get_user_model()
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
+    # Prevent signal from running during migrations or createsuperuser
+    if 'makemigrations' in sys.argv or 'migrate' in sys.argv or 'createsuperuser' in sys.argv:
+        return
+
     if created:
         Participant.objects.create(user=instance)
